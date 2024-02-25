4f971d64d0bf35f11e513613aec109801609dece
wsdwasdwawdw <rosales.186476@sjdelmonte.sti.edu.ph>
2024-02-26 02:45:09 +0800

    adjustments to mobile version

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const p1 = document.querySelector(".project1");
+const menu = document.querySelector(".menu");
+const burger = document.querySelector(".burger");
+const x = document.querySelector(".x");
+
 
 p1.addEventListener("click", ()=>{
     window.open("projects/project1/index.html", "_blank");
+});
+
+
+menu.addEventListener("click", ()=>{
+    burger.style.right = "5px";
+});
+
+x.addEventListener("click", ()=>{
+    burger.style.right = "-100px";
 });
